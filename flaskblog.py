c3f97e8bb62fd87cee9ffd673032dd16234a5182
JQ4386 <jingqi_qu@berkeley.edu>
2023-07-11 23:14:41 +1000

    Set up SQLAlchemy db structure for User and Post

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,37 @@
-from flask import Flask, render_template, flash, redirect, url_for
+from datetime import datetime
+from flask import Flask, render_template, flash, redirect, url_for, current_app
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+import secrets
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'a1ba33631eaf7e72e61b84652553a933a185d5e9'
+app.secret_key = 'a1ba33631eaf7e72e61b84652553a933a185d5e9'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+# models
+class User(db.Model):
+    # columns
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False) # 20 char max, unique, not null
+    email = db.Column(db.String(120), unique=True, nullable=False) # 120 char max, unique, not null
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg') # 20 char max, not null, default
+    password = db.Column(db.String(60), nullable=False) # 60 char max, not null
+    posts = db.relationship('Post', backref='author', lazy=True) # one to many relationship, backref allows us to get user who created post
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+    
+class Post(db.Model):
+    # columns
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False) # 100 char max, not null
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) # not null, default, always use utc time for consistency
+    content = db.Column(db.Text, nullable=False) # not null
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) # not null
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 posts = [
     {
