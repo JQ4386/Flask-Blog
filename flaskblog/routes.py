77e5dc2acab37d1511e6a7ad93944a71b10c7dcf
JQ4386 <jingqi_qu@berkeley.edu>
2023-07-18 23:15:08 +1000

    implement Account page + Basic detail update

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -11,7 +11,7 @@ posts = [
         'title': 'Blog Post 1',
         'content': 'First post content',
         'date_posted': 'April 20, 2018',
-    }, 
+    },
     {
         'author': 'Jane Doe',
         'title': 'Blog Post 2',
@@ -23,15 +23,20 @@ posts = [
 # routes - url paths for our app to navigate to different pages
 
 # home page
+
+
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', title='Home', posts=posts)
 
 # about page
+
+
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -41,15 +46,18 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         # hash password
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
         # create user
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        user = User(username=form.username.data,
+                    email=form.email.data, password=hashed_password)
         # add user to db
         db.session.add(user)
         db.session.commit()
         flash(f'Your account has been create! You are now able to log in!', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -58,7 +66,7 @@ def login():
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-         # check if user exists
+        # check if user exists
         user = User.query.filter_by(email=form.email.data).first()
 
         # if user exists and password matches
@@ -80,7 +88,24 @@ def logout():
     return redirect(url_for('home'))
 
 # account route
-@app.route('/account')
+
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
+# login_required decorator ensures user is logged in before accessing account page
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        # update username and email
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        # commit changes to db
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account')) # Avoid Post/Redirect/Get pattern
+    elif request.method == 'GET':
+        # populate form with current user info
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
